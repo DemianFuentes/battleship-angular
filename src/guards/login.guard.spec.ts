ae102ae35dd09ddee7fcf02dae7edc332e267564
DemianFuentes <demian.fuentes@teracode.com>
2021-03-17 10:35:45 -0300

    login-guard tests added

--- src/guards/login.guard.spec.ts
+++ src/guards/login.guard.spec.ts
@@ -5,6 +5,8 @@ import { PlayerService } from '../services/player.service';
 import { StorageService } from '../services/storage.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { Player } from '../model/player';
+import { Router } from '@angular/router';
 
 describe('LoginGuard', () => {
   beforeEach(() => {
@@ -23,7 +25,26 @@ describe('LoginGuard', () => {
     });
   });
 
-  it('should ...', inject([LoginGuard], (guard: LoginGuard) => {
+  it('should reject not logged in user', inject([LoginGuard], (guard: LoginGuard) => {
+    const playerService: PlayerService = TestBed.get(PlayerService);
+    spyOn(playerService, 'getCurrentPlayer').and.returnValue(null);
+
+    const router = TestBed.get(Router);
+    spyOn(router, 'navigate');
+
     expect(guard).toBeTruthy();
+    expect(guard.canActivate(null, null)).toBeFalsy();
+    expect(playerService.getCurrentPlayer).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  }));
+
+  it('should not reject logged in user', inject([LoginGuard], (guard: LoginGuard) => {
+    const playerService: PlayerService = TestBed.get(PlayerService);
+    const dummyPlayer = new Player({name: 'dummy', lang: 'es'});
+    spyOn(playerService, 'getCurrentPlayer').and.returnValue(dummyPlayer);
+
+    expect(guard).toBeTruthy();
+    expect(guard.canActivate(null, null)).toBeTruthy();
+    expect(playerService.getCurrentPlayer).toHaveBeenCalled();
   }));
 });
